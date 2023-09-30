cc44498eafbd7d44bb9e4b6852b9a48c613a15aa
fahmidanial <naloniel2000@gmail.com>
2023-09-30 11:58:22 +0800

    resize sidebar img

--- src/post/Post.jsx
+++ src/post/Post.jsx
@@ -19,6 +19,9 @@ export default function Post() {
             <hr/>
             <span className="postDate">1 hour ago</span>
         </div>
+        <p className="postDesc">
+        Lorem feugiat et esse diam magna et autem ipsum. In clita enim in vero stet sed labore eos et velit. Assum tempor ipsum diam labore amet esse clita et. No sit tempor diam kasd feugiat quis eirmod rebum. No et takimata justo duo stet mazim. Rebum dolore nulla ad justo et diam et. Et sit et gubergren sed adipiscing sea ipsum voluptua liber labore sit. Dignissim ex takimata ipsum velit eos aliquyam labore eos. Amet duo dolor justo ea enim nibh sed esse. Lorem ea elitr sadipscing eu sit feugiat et ea sit sea sea aliquam tempor. Kasd liber accusam ut dolores quod lorem gubergren. Ipsum elitr sadipscing amet elitr sea facilisis et delenit sed eu invidunt sit. Amet invidunt at magna dolor diam dolor clita dolores sanctus takimata lorem sea ut clita invidunt stet labore.
+        </p>
     </div>
   )
 }
