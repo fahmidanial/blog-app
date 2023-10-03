9811ad02f981b1ac0ff2dd304a1e98f9d51218bf
fahmidanial <naloniel2000@gmail.com>
2023-10-04 00:31:23 +0800

    route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import TopBar from "./components/topbar/TopBar";
 import Login from "./pages/login/Login";
-// import Settings from "./pages/settings/Settings";
-// import Single from "./pages/single/Single";
-// import Write from "./pages/write/Write"
+import Settings from "./pages/settings/Settings";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write"
 
 function App() {
   return (
     <>
       <TopBar />
       <Login />
+      <Settings />
+      <Single />
+      <Write />
     </>
   );
 }
