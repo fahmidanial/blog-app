f438a63b03b8787900b1a87c07633fe2e3d06810
fahmidanial <naloniel2000@gmail.com>
2023-10-03 20:07:02 +0800

    register

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -1,16 +1,38 @@
 import "./settings.css"
 import Sidebar from "../../components/sidebar/Sidebar"
+import pxl from '../../assets/pxl.png';
+
 
 export default function Setting() {
   return (
     <div className="settings">
-        <div className="settingWrapper"></div>
+      <div className="settingsWrapper">
         <div className="settingsTitle">
           <span className="settingsUpdateTitle">Update Your Account</span>
           <span className="settingsDeleteTitle">Delete Account</span>
         </div>
-        
-        <Sidebar/>
+        <form className="settingsForm">  
+            <label>Profile Picture</label>
+            <div className="settingsPP">
+              <img 
+                src={pxl} 
+                alt="pxl"
+              />
+              <label htmlFor="fileInput">
+                <i class="settingsPPIcon fa-solid fa-users"></i>  
+              </label>
+              <input type="file" id="fileInput" style={{display: "none"}}/>
+            </div>
+            <label>Username</label>
+            <input type="text" placeholder="fahmidanial9091@gmail.com"/>
+            <label>Email</label>
+            <input type="text" placeholder="fahmidanial9091@gmail.com"/>
+            <label>Password</label>
+            <input type="password"/>
+            <button className="settingsSubmit">Update</button>
+        </form>
+      </div>
+      <Sidebar/>
     </div>
   )
 }
